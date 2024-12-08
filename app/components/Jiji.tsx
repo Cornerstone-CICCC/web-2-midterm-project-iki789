d9b4e43b4180f4fe5305e0b63dc17817a9d9cfc1
Ikhlak <ikhlak789@gmail.com>
2024-12-08 11:19:52 -0800

    jiji's menu

--- app/components/Jiji.tsx
+++ app/components/Jiji.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const stateMap = {
@@ -28,6 +29,17 @@ const stateMap = {
   },
 };
 
+const sleepUrls = [
+  { text: "･･「マグロ」", url: "" },
+  { text: "･🍖「さしみ」･･", url: "" },
+  { text: "「カルカン」🐟･", url: "" },
+];
+
+const urls = [
+  { text: "Home", url: "/" },
+  { text: "About Us", url: "/about" },
+];
+
 const Jiji = () => {
   const [clickCount, setClickCount] = useState(0);
   const [currentState, setCurrentState] = useState<{
@@ -36,6 +48,7 @@ const Jiji = () => {
     length?: number;
   }>();
   const [wait, setWait] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
   useEffect(() => {
     (function () {
@@ -58,7 +71,7 @@ const Jiji = () => {
           }
         }
 
-        if (currentState != stateMap[3]) {
+        if (currentState != stateMap[3] || currentState != stateMap[4]) {
           if (clickCount >= 21) {
             setCurrentState(stateMap[3]);
             setTimeout(() => {
@@ -69,19 +82,68 @@ const Jiji = () => {
         }
       }
     })();
-    console.log(clickCount, currentState);
   }, [clickCount]);
+
+  const Menu = (
+    <div className="relative rounded-xl z-[1] w-[180px] h-[140px] pb-7 top-[36px] left-[-50px] bg-[url('/bubble.png')] bg-contain">
+      <div
+        className="p-4 text-black z-[1] w-full"
+        style={{
+          writingMode:
+            currentState !== stateMap[3] && currentState !== stateMap[4]
+              ? "vertical-rl"
+              : "unset",
+        }}
+      >
+        {currentState === stateMap[2] &&
+          sleepUrls.map((item) => (
+            <div key={`${item.text} sleepyUrl`} className="font-bold">
+              <a href="#">{item.text}</a>
+            </div>
+          ))}
+
+        {currentState === stateMap[3] || currentState === stateMap[4] ? (
+          <div className="text-center">
+            <div className="font-bold mb-1 underline">Menu</div>
+            {urls.map((item) => (
+              <Link
+                key={`${item.text} urls`}
+                href={item.url}
+                className="block mb-1"
+              >
+                {item.text}
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+    console.log(currentState === stateMap[4], currentState, stateMap[3]);
+    if (
+      currentState === stateMap[2] ||
+      currentState === stateMap[3] ||
+      currentState === stateMap[4]
+    ) {
+      setShowMenu(!showMenu);
+    }
+  };
 
   return (
     <div className="fixed bottom-0 right-0 z-[31]">
-      <div>Menu</div>
-      <div>
+      {showMenu ? Menu : <></>}
+      <div className="flex justify-end">
         <img
           role="button"
           className="h-[150px] object-fill"
           src={currentState?.url}
           alt={currentState?.alt}
-          onClick={() => setClickCount(clickCount + 1)}
+          onClick={handleClick}
         />
       </div>
     </div>
